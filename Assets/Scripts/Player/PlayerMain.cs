01190febaac6286311d3adb0be3fb4f07a717289
Pedro Augusto Ikeoka <pedroikeoka@gmail.com>
2023-10-23 12:44:54 -0300

    Coisinhas 2

--- Assets/Scripts/Player/PlayerMain.cs
+++ Assets/Scripts/Player/PlayerMain.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMain : MonoBehaviour {
+public class PlayerMain : MonoBehaviour
+{
 
     //Script principal de controle para o player
-    private enum State {
+    private enum State
+    {
         Normal,
         Dodging,
+        BouttaSprint,
+        Sprinting,
+        Wounded,
     }
 
     [SerializeField]
@@ -19,6 +24,11 @@ public class PlayerMain : MonoBehaviour {
     private float dodgeForceDrop;
     private int dodgeCount = 0;
     private float dodgeSpeed;
+    private bool isSprinting = false;
+    [SerializeField]
+    private float sprintSpeed;
+
+    public Animator anim;
 
     private Vector2 movementInput; //input do jogador
 
@@ -41,77 +51,93 @@ public class PlayerMain : MonoBehaviour {
 
     }
 
-    private void Update() {
-        switch (state) {
+    private void Update()
+    {
+        switch (state)
+        {
             case State.Normal:
                 break;
             case State.Dodging:
                 dodgeSpeed -= dodgeSpeed * dodgeForceDrop * Time.deltaTime;
-                
+
                 float dodgeSpeedMinimum = 6f;
-                if (dodgeSpeed < dodgeSpeedMinimum ) {
+                if (dodgeSpeed < dodgeSpeedMinimum)
+                {
                     state = State.Normal;
                     rb.velocity = Vector2.zero;
                 }
                 break;
+            case State.BouttaSprint:
+                break;
+            case State.Sprinting:
+                break;
+
         }
     }
 
-    private void FixedUpdate() { //Fixed Update pra movimentacao n depender da taxa d quadros do jogo
+    private void FixedUpdate()
+    { //Fixed Update pra movimentacao n depender da taxa d quadros do jogo
 
-        switch (state) {
+        switch (state)
+        {
             case State.Normal:
-                if (movementInput != Vector2.zero) {
+                anim.SetBool("isDodging", false);
+                if (movementInput != Vector2.zero)
+                {
                     bool success = TryMove(movementInput);
 
-                    if (!success) {
+                    anim.SetFloat("velocity", speed);
+
+                    if (!success)
+                    {
                         success = TryMove(new Vector2(movementInput.x, 0)); //se o player estiver encostado numa parede ele ainda pode deslizar verticalmente
 
-                        if (!success) {
+                        if (!success)
+                        {
                             success = TryMove(new Vector2(0, movementInput.y)); //mesma coisa mas horizontalmente
                         }
                     }
 
                 }
 
-                if (movementInput != Vector2.zero) {
-                    bool success = TryMove(movementInput);
-
-                    if (!success) {
-                        success = TryMove(new Vector2(movementInput.x, 0)); //se o player estiver encostado numa parede ele ainda pode deslizar verticalmente
-
-                        if (!success) {
-                            success = TryMove(new Vector2(0, movementInput.y)); //mesma coisa mas horizontalmente
-                        }
-                    }
-
+                if (movementInput == Vector2.zero)
+                {
+                    anim.SetFloat("velocity", 0);
                 }
 
                 break;
 
             case State.Dodging:
 
-                
-                
+                anim.SetBool("isDodging", true);
 
-                if (movementInput != Vector2.zero) {
+
+                if (movementInput != Vector2.zero)
+                {
                     rb.velocity = movementInput * dodgeSpeed;
 
                     bool success = TryDodge(movementInput);
 
-                    if (!success) {
+                    if (!success)
+                    {
                         rb.velocity = Vector2.zero;
                         state = State.Normal;
                     }
 
                 }
-                
-
-                
 
 
 
 
+
+
+
+
+                break;
+            case State.BouttaSprint:
+                anim.SetBool("isSprinting", true);
+                break;
+            case State.Sprinting:
 
                 break;
         }
@@ -120,31 +146,39 @@ public class PlayerMain : MonoBehaviour {
 
     }
 
-    private bool TryMove(Vector2 direction) { //metodo pra detectar colisoes
+    private bool TryMove(Vector2 direction)
+    { //metodo pra detectar colisoes
         int count = rb.Cast(
                 direction, //direcao do input
                 movementFilter,
                 castCollisions,
                 speed * Time.fixedDeltaTime + collisionOffset); //distancia dos raycasts 
 
-        if (count == 0) { //se o player n estiver encostado em nada ele pode se mover
+        if (count == 0)
+        { //se o player n estiver encostado em nada ele pode se mover
             rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
             return true;
-        } else {
+        }
+        else
+        {
             return false;
         }
     }
 
-    private bool TryDodge(Vector2 direction) {
+    private bool TryDodge(Vector2 direction)
+    {
         int count = rb.Cast(
             direction,
             movementFilter,
             castCollisions,
             dodgeSpeed * Time.fixedDeltaTime + collisionOffset);
 
-        if (count > 0) {
+        if (count > 0)
+        {
             return false;
-        } else {
+        }
+        else
+        {
             return true;
         }
 
@@ -152,12 +186,15 @@ public class PlayerMain : MonoBehaviour {
 
 
 
-    void OnMove(InputValue inputValue) { //detecta o input do jogador (metodo derivado do InputSystem)
+    void OnMove(InputValue inputValue)
+    { //detecta o input do jogador (metodo derivado do InputSystem)
         movementInput = inputValue.Get<Vector2>();
     }
 
-    void OnDodge(InputValue inputValue) {
-        if (dodgeCount == 0) {
+    void OnDodge(InputValue inputValue)
+    {
+        if (dodgeCount == 0)
+        {
             dodgeCount++;
             state = State.Dodging;
             dodgeSpeed = dodgeForce;
@@ -167,14 +204,45 @@ public class PlayerMain : MonoBehaviour {
 
     }
 
+    void OnSprint(InputValue inputValue)
+    {
+        if (!isSprinting)
+        {
+
+            state = State.BouttaSprint;
+            StartCoroutine(SprintTimer());
+            isSprinting = true;
+        }
+        else if (isSprinting)
+        {
+
+            anim.SetBool("isSprinting", false);
+            StartCoroutine(SprintTimer());
+            isSprinting = false;
+        }
+
+    }
 
 
-    IEnumerator DodgeTimer() {
-        if (dodgeCount != 0) {
+    IEnumerator DodgeTimer()
+    {
+        if (dodgeCount != 0)
+        {
             yield return new WaitForSeconds(2);
             dodgeCount = 0;
         }
     }
 
+    IEnumerator SprintTimer()
+    {
+        yield return new WaitForSeconds(2);
+        state = State.Sprinting;
+    }
+
+    IEnumerator WindDown()
+    {
+        yield return new WaitForSeconds(2);
+        state = State.Normal;
+    }
 
 }
