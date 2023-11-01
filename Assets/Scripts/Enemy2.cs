1772041f1d8b39944a92adf3bb6260099e02fbfd
Pedro Augusto Ikeoka <pedroikeoka@gmail.com>
2023-11-01 10:41:36 -0300

    enemy movement

--- Assets/Scripts/Enemy2.cs
+++ Assets/Scripts/Enemy2.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy2 : MonoBehaviour
@@ -12,14 +13,16 @@ public class Enemy2 : MonoBehaviour
         Damaged,
     }
 
-    [SerializeField]
-    private int hitPoints = 0;
+    //[SerializeField]
+    //private int hitPoints = 0;
 
     public GameObject Player;
     private Vector3 playerPos;
-    private Vector2 endPos;
+    private float difX;
+    private float difY;
+    private Vector2 moveDir;
 
-
+    [SerializeField]
     private float enemySpeed;
 
     private Rigidbody2D rb;
@@ -29,9 +32,11 @@ public class Enemy2 : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        Player = GameObject.FindGameObjectWithTag("Player");
         state = EnemyState.Idle;
     }
+
+    
 
     private void FixedUpdate()
     {
@@ -40,18 +45,41 @@ public class Enemy2 : MonoBehaviour
         if (Player != null)
         {
             playerPos = Player.transform.position;
-            endPos = transform.position - playerPos;
+            
+            difX = playerPos.x - transform.position.x;
+            difY = playerPos.y - transform.position.y;
 
+            if (difX > 0 && difY > 0)
+            {
+                moveDir = Vector2.one;
+            }
+
+            if (difX < 0 && difY < 0)
+            {
+                moveDir = -Vector2.one;
+            }
+
+            if (difX < 0 && difY > 0)
+            {
+                moveDir = new Vector2(-1, 1);
+            }
+
+            if (difX > 0 && difY < 0)
+            {
+                moveDir = new Vector2(1, -1);
+            }
+
+            
 
         }
 
         switch (state)
         {
             case EnemyState.Idle:
-
+                StartCoroutine(Idle());
                 break;
             case EnemyState.Moving:
-
+                StartCoroutine(Move());
                 break;
 
         }
@@ -62,11 +90,18 @@ public class Enemy2 : MonoBehaviour
     {
         
         yield return new WaitForSeconds(1);
+        rb.AddForce(moveDir * enemySpeed);
+        yield return new WaitForSeconds(1);
+        rb.velocity = Vector2.zero;
+        state = EnemyState.Idle;
 
     }
 
     IEnumerator Idle()
     {
+
+        yield return new WaitForSeconds(2);
+        state = EnemyState.Moving;
 
     }
 
